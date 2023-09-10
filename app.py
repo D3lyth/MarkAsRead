d0105990f63726d6615bbd20d4fc6b993ba32499
D3lyth <>
2023-09-10 18:08:36 +0000

    Create registration and log in forms

--- app.py
+++ app.py
@@ -23,3 +23,21 @@ class User(UserMixin):
         self.email = email
         self.password = password
 
+
+# Create registration and login forms
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[
+                           DataRequired(), Length(min=4, max=25)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[
+                             DataRequired(), Length(min=6)])
+    confirm_password = PasswordField('Confirm Password', validators=[
+                                     DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
